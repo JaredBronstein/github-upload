2973c2932f69c7146e537ba0e6ff35e831852f63
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-09-29 17:55:49 -0500

    Added Stuff

---
+++ Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,47 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] points;
+    private int destPoint = 0;
+    
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        else if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
